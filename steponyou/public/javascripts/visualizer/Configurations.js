b02833127eb6943526551c3531848c7fcb2cca98
Victor <victorpham93@gmail.com>
2015-02-05 18:52:56 +0800

    Integrate Client into server

---
+++ steponyou/public/javascripts/visualizer/Configurations.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Configurations = {
+	canvasWidth: 800,
+	canvasHeight: 600,
+	characterWidth: 100,
+	characterHeight: 100,
+	
+	KEYWORD_UPDATE: "update",
+	KEYWORD_CREATE: "create",
+	KEYWORD_REMOVE: "remove"
+};
+
+var ASSET_PREFIX = 'images/';
+var ASSET_LIST = {
+	background: 'background3.jpg',
+	character_devil: 'Character_Devil.png',
+	character_angel: 'Character_Angel.png',
+	character_chicken: 'Character_Chicken.png',
+	character_green: 'Character_Green.png',
+	character_white: 'Character_White.png'
+};
+
+function AssetManager(assetList) {
+  this.assetList = (!assetList) ? ASSET_LIST : assetList;
+}
+
+AssetManager.prototype.load = function(callback) {
+	var loadedImages = 0;
+	var numImages = 0;
+	// get num of sources
+	for(var src in this.assetList) {
+	  numImages++;
+	}
+	for(var src in this.assetList) {
+	  this[src] = new Image();
+	  this[src].onload = function() {
+		if(++loadedImages >= numImages) {
+		  callback();
+		}
+	  };
+	  this[src].src = ASSET_PREFIX + this.assetList[src];
+	}
+};

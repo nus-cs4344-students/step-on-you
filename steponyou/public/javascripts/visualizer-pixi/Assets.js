2a30a88f1599d708a8581a370bdb10cde5f1bb3c
Sean Nguyen <seannguyen@r-61-177-28-172.comp.nus.edu.sg>
2015-02-11 12:39:14 +0800

    changing render framework from KineticJS to PixiJS

---
+++ steponyou/public/javascripts/visualizer-pixi/Assets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var ASSET_PREFIX = 'gameImages/';K
+var ASSET_LIST = {
+	background: ASSET_PREFIX + 'background3.jpg',
+	character_devil: ASSET_PREFIX + 'Character_Devil.png',
+	character_angel: ASSET_PREFIX + 'Character_Angel.png',
+	character_chicken: ASSET_PREFIX + 'Character_Chicken.png',
+	character_green: ASSET_PREFIX + 'Character_Green.png',
+	character_white: ASSET_PREFIX + 'Character_White.png'
+};
---
+++ steponyou/public/javascripts/visualizer-pixi/Configurations.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var Configurations = {
+	//dimensions
+	canvasWidth: 800,
+	canvasHeight: 600,
+	characterWidth: 100,
+	characterHeight: 100,
+	
+	//Strings
+	KEYWORD_UPDATE: "update",
+	KEYWORD_CREATE: "create",
+	KEYWORD_REMOVE: "remove",
+
+	//colors
+	colors: {
+		background: 0xEEEEEE
+	}
+};

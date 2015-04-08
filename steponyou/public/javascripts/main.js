d39b84771fb905b2dc4bd0e95755de51ec66caf8
Paulina Choo <paulinachoo92@gmail.com>
2015-04-08 18:23:59 +0800

    mobile controls

--- steponyou/public/javascripts/main.js
+++ steponyou/public/javascripts/main.js
@@ -57,7 +57,6 @@ var updateVisualizer = function(){
 var lobbyManager = new LobbyManager();
 lobbyManager.startConnection(setupGameEngin);
 
-
 var handleKey = function(e){
     //Player is not playing, don't need to handle his key event
     if(!gameEngine.isPlaying){
@@ -75,3 +74,128 @@ var handleKey = function(e){
 	lobbyManager.sendEvent(lobbyManager.playerId, playerEvent);
 
 }
+
+//mobile controls
+var tiltLeft = false;
+var tiltRight = false;
+var tapJump = false;
+
+var rotationCenter = 0;
+var accelerometer  = function(event){
+	var g = [];
+	g[0] = event.accelerationIncludingGravity.x;
+	g[1] = event.accelerationIncludingGravity.y;
+	g[2] = event.accelerationIncludingGravity.z;
+
+	var norm_Of_g = Math.sqrt(g[0] * g[0] + g[1] * g[1] + g[2] * g[2]);
+
+	// Normalize the accelerometer vector
+	g[0] = g[0] / norm_Of_g;
+	g[1] = g[1] / norm_Of_g;
+	g[2] = g[2] / norm_Of_g;
+	
+	var rotation = Math.atan2(g[1], g[0]);
+	if(rotationCenter < rotation){ //tilt right?
+		tiltLeft = false;
+		tiltRight = true;
+	}
+	else if(rotation < rotationCenter){ //tilt left??
+		tiltLeft = true;
+		tiltRight = false;
+	}
+	else{
+		tiltLeft = false;
+		tiltRight = false;
+	}
+	convertMobileEvent();
+}
+
+var startAlpha;
+var centerGamma = 0;
+var magnetometer  = function(event){
+	if(startAlpha == null){ //init start compass
+		startAlpha = event.alpha;
+	}
+//	if(event.beta < 25 || event.beta > 155){ //device is flat
+//		var rotate;
+//		if(event.alpha
+//		
+//	}
+//	else{ //device is not flat
+		if(event.gamma < centerGamma){ //tilt left
+			tiltLeft = true;
+			tiltRight = false;
+		}
+		else if(event.gamma > centerGamma){ //tilt right
+			tiltLeft = false;
+			tiltRight = true;
+		}
+		else{ //neutral position
+			tiltLeft = false;
+			tiltRight = false;
+		}
+		convertMobileEvent();
+//	}
+}
+
+var startJump = function(){
+	tapJump = true;
+	convertMobileEvent();
+}
+
+var endJump = function(){
+	tapJump = false;
+	convertMobileEvent();
+}
+
+var convertMobileEvent = function(){
+	keyMap[37] = tiltLeft;
+	keyMap[38] = tapJump;
+	keyMap[39] = tiltRight;
+	
+    var playerEvent = { keyMap : keyMap,
+                        pos :  thisPlayer.getPosition() };
+    gameEngine.simulatePlayer(lobbyManager.playerId, playerEvent);
+	lobbyManager.sendEvent(lobbyManager.playerId, playerEvent);
+}
+
+var previousOrientation = window.orientation;
+var checkOrientation = function(){
+    if(window.orientation !== previousOrientation){
+        previousOrientation = window.orientation;
+		console.log(previousOrientation);
+		if(previousOrientation == 0){ //portrait
+			rotationCenter = 0;
+			centerGamma = 0;
+		}
+		else if(previousOrientation == -90){ //landscape rotate right
+			rotationCenter = -90;
+			centerGamma = 90;
+		}
+		else if(previousOrientation == 90){ //landscape rotate left
+			rotationCenter = 90;
+			centerGamma = -90;
+		}
+    }
+};
+
+//add mobile event listeners
+if(document.DeviceMotionEvent){
+	document.addEventListener("devicemotion", accelerometer, false);
+}else{
+	console.log("DeviceMotionEvent is not supported");
+}
+
+if(window.DeviceOrientationEvent){
+	window.addEventListener("deviceorientation", magnetometer, false);
+}else{
+	console.log("DeviceOrientationEvent is not supported");
+}
+
+window.addEventListener("touchstart", startJump, false);
+window.addEventListener("touchmove", startJump, false);
+window.addEventListener("touchend", endJump, false);
+window.addEventListener("touchcancel", endJump, false);
+
+window.addEventListener("resize", checkOrientation, false);
+window.addEventListener("orientationchange", checkOrientation, false);

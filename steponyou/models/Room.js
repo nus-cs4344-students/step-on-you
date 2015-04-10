15ade96e03537b68565b5b844ac2a11ed258d0c6
strydfelt <strydfelt@gmail.com>
2015-04-10 21:23:03 +0800

    commiting to pull. attempt to implement rewinding

--- steponyou/models/Room.js
+++ steponyou/models/Room.js
@@ -7,10 +7,14 @@ module.exports = function Room(rmID){
 	this.players = {};//index via playerid
 	this.sockets = {};//index via playerid
 	this.gameEngine.init(null);
-	this.gameEngine.start();
+	//this.gameEngine.start();
+
 	var that = this;
+
 	this.states = [];
 	this.inputHistory = [];
+	this.maxStateHistory = 30;
+
 	this.playerLatency = [];
 	this.playerLag = [];
 
@@ -59,8 +63,110 @@ module.exports = function Room(rmID){
 	};
 
 	this.updatePlayer = function(playerID, message, timestamp){
-		this.gameEngine.simulatePlayer(playerID, message, timestamp);
+
+		gameEngine.stop();
+		var packet = {playerID:playerID, message:message, timeStamp:timestamp};
+		var cIndex = -1;
+		var eIndex = -1;
+		//if first element
+		if(this.inputHistory.length == 0 ||(this.inputHistory[inputHistory.length-1].timeStamp <= timestamp )) {
+			this.inputHistory.push(packet);
+			cIndex = this.inputHistory.length - 1;
+			eIndex = this.states.length - 1;
+
+		}
+		else{
+			//determine where to insert the new packet
+			for(var i = 0; i < this.inputHistory.length; i++){
+				if(this.inputHistory[i].timeStamp >= timestamp){
+					cIndex = i;
+					break;
+				}
+			}
+
+			//insert the new packet
+			this.inputHistory.splice(cIndex,0, packet );
+
+			//determine index of engine
+			//scan from the back
+			for(var i = this.states.length-1; i > 0; i--){
+				if(this.states[i].time <= timestamp){
+					eIndex = i;
+					break;
+				}
+			}
+
+
+		}
+			
+
+		//now we have identified the engine to use and the starting point of the input to run from
+		//this may be the last engine state and only 1 input to run
+
+		var overwriteEIndex = eIndex + 1;
+		var engineTime = this.states[eIndex].time;
+		//for all inputs to fastforward / run
+		for(var i = cIndex; i < this.inputHistory.length; i++ ){
+
+			var currentInput = this.inputHistory[i];
+			var timeGap =  engineTime - currentInput.timeStamp;
+
+			//number of times to fast forward the engine to closest time to run the input
+			var numFFSteps = timeGap / this.gameEngine.timePerFrame;
+			//"fastforward" the gameEngine by stepping
+			for(var j = 0; j < numFFSteps; j++){
+				engineTime += this.gameEngine.timePerFrame;
+				stepEngine(overwriteEIndex++, engineTime);
+			}
+			//run the simulation for the player
+			this.gameEngine.simulatePlayer(packet.playerID, packet.message, packet.timestamp);
+			engineTime += this.gameEngine.timePerFrame;
+			stepEngine(overwriteEIndex++, engineTime);
+		}
+
+		//this.gameEngine.simulatePlayer(playerID, message, timestamp);
 	};
+
+	var runEngine = false;
+
+	//save game engine states AFTER simulation to be sure that no more player inputs exist at that time
+	//replaceIndex is the index to override if needed
+	var stepEngine = function(replaceIndex, overrideTime){
+		this.gameEngine.step();
+		if(overrideTime == null){
+			overrideTime = (new Date()).getTime();
+		}
+		var state = {engine:gameEngine, time: overrideTime};
+		if(replaceIndex == null){
+			this.states.push(state);
+			if(this.states.length > this.maxStateHistory ){
+				this.states.shift();
+			}
+		}
+		else{
+			if(this.states.length > replaceIndex || (this.states.length == this.maxStateHistory && replaceIndex < this.maxStateHistory)){
+				this.states[replaceIndex] = state;
+			}
+			else{
+				console.log("You should not be seeing this message");
+				console.log("replaceIndex: " + replaceIndex + "  states size: " + this.states.length);
+				console.log("Attempting recovery methods - may fail");
+				this.states.push(state);
+			}
+
+		}
+	}
+
+	var loopEngine = function(){
+		if(runEngine){
+			stepEngine();
+			setTimeout(function(){loopEngine();}, this.gameEngine.timePerFrame);
+		}
+	}
+
+	var stopLoopEngine = function(){
+		runEngine = false;
+	}
 
 	this.generateUpdateState = function(){
 		var updatePack = this.gameEngine.generateUpdate();

7962defbfb61442810ceb20d32a0d1fec15409c9
Victor <victorpham93@gmail.com>
2015-04-12 15:35:45 +0800

    Disable join room button after click

--- steponyou/public/javascripts/Events.js
+++ steponyou/public/javascripts/Events.js
@@ -2,6 +2,7 @@ var onRoomButtonClick = function(roomId) {
 	var currentRoomId = lobbyManager.roomId;
 	if (currentRoomId == -1 && roomId > -1) {
 		lobbyManager.joinRoom(roomId);
+ 		document.getElementById("buttonRoom"+roomId).blur();
 	} else if (currentRoomId > -1 && currentRoomId == roomId) {
 		lobbyManager.leaveRoom();
 	}
